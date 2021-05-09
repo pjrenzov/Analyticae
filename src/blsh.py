7799187c48b393eec7677e8982a25f6995b58a6a
paras jadiya <parasjadiya@icloud.com>
2021-05-10 00:00:02 +0530

    updated BLSH

--- src/blsh.py
+++ src/blsh.py
@@ -5,6 +5,7 @@ from numpy import where
 class BLSH(Stock):
     def __init__(self):
         self.signal = None
+        self.portfolio = None
     
     def generateSignal(self,start_date=self.start_date,end_date=self.end_date):
         stock_signal = DataFrame(index=self.stock_data.index)
@@ -28,3 +29,28 @@ class BLSH(Stock):
         temp_sig = self.signal[self.signal['Position']!=0.0]
         return self.Call(temp_sig.index[-1])
 
+    def makePortfolio(self,start=self.start_date,end=self.end_date,capital = None):
+        if not capital:
+            capital = self.stock_signal.loc[start,'Price']
+        positions = pd.DataFrame(index=self.signal.index).fillna(0.0) 
+        portfolio = pd.DataFrame(index=self.signal.index).fillna(0.0)
+        positions[self.symbol] = self.signal['Signal']
+        portfolio['postions'] = (positions.multiply(self.signal['Price'], axis=0)) 
+        portfolio['cash'] = capital - (positions.diff().multiply(self.signal['Price'], axis=0)).cumsum()
+        portfolio['total'] = portfolio['positions'] + portfolio['cash']
+        self.portfolio = portfolio
+    
+    def profit(self,portfolio=self.portfolio,intial_capital=None):
+        if not intial_capital:
+            intial_capital = self.stock_signal.loc[start,'Price']
+        if not portfolio:
+            self.makePortfolio()
+        return portfolio.iloc[len(portfolio['cash'])-1][2]-intial_capital
+    
+    def percentageReturn(self,profit=None,intial_capital=None):
+        if not intial_capital:
+            intial_capital = self.stock_signal.loc[start,'Price']
+        if not profit:
+            profit = self.profit(intial_capital=intial_capital)
+        return float((profit/intial_capital)/100)
+        
