63279646e8adbe29882a5d229b3d2e65a0dee0f2
paras jadiya <parasjadiya@icloud.com>
2021-05-09 23:38:53 +0530

    updated BLSH

--- src/blsh.py
+++ src/blsh.py
@@ -1,5 +1,6 @@
 from stock import Stock
 from pandas import DataFrame
+from numpy import where
 
 class BLSH(Stock):
     def __init__(self):
@@ -9,6 +10,21 @@ class BLSH(Stock):
         stock_signal = DataFrame(index=self.stock_data.index)
         stock_signal['Price'] = self.stock_data['Adj Close']
         stock_signal['Price Difference'] = self.stock_data['Adj Close'].diff()
+        stock_signal['Signal'] = 0
+        stock_signal['Signal'] = where(stock_signal['Price Difference']>=0,1.0,0.0)
+        stock_signal['Position'] = stock_signal['Signal'].diff()
         self.signal = stock_signal
 
+    def Call(self,date = self.end_date):
+        if self.signal.loc[date,'Position'] == -1.0:
+            return "Sell"
+        elif self.signal.loc[date,'Position'] == 1.0:
+            return "Buy"
+        else:
+            return "Hold"
     
+    def mostRecentCall(self,date=self.end_date):
+        temp_sig = self.signal[:date]
+        temp_sig = self.signal[self.signal['Position']!=0.0]
+        return self.Call(temp_sig.index[-1])
+
